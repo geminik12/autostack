8a332383896b614df69c7e984e4eef6e7024a67b
Liangkang Zhang <gemini12@163.com>
2026-01-27 00:34:32 +0800

    feature: update go.mod

--- authz/authz.go
+++ authz/authz.go
@@ -2,7 +2,7 @@
  * @Author: Liangkang Zhang
  * @Date: 2026/1/27 00:18:55
  * @LastEditors: Liangkang Zhang
- * @LastEditTime: 2026/1/27 00:19:36
+ * @LastEditTime: 2026/1/27 00:23:22
  * @Description:
  * @Copyright: Copyright (©)}) 2026 Liangkang Zhang<lkzhang98@gmail.com>. All rights reserved. Use of this source code is governed by a MIT style license that can be found in the LICENSE file.. All rights reserved.
  * @Email: lkzhang98@gmail.com
